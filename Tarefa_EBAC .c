92452cfe0783a316a910eff00ae41be05752a0c1
ygor-dev0 <ygorsilva.00@hotmail.com>
2024-09-02 15:02:45 -0400

    Opção de saída
    opção de saída incluso para o usuário

--- Tarefa_EBAC .c
+++ Tarefa_EBAC .c
@@ -124,6 +124,7 @@ int main ()
 	  printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
 	  printf("\t3 - Deletar nomes\n\n"); 
+	  printf("\t4 - Sair do sistema\n\n");
 	  printf("Opção :");// fim do menu
    
       scanf("%d" , &opcao); // armazenando a escolha do usuario
@@ -144,6 +145,11 @@ int main ()
    	    deletar();
    	    break;
    	    
+   	    case 4:
+   	    printf("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
+		   	
    	    default:
    	    printf("Essa opção não está disponivel!\n");
     	system("pause");
